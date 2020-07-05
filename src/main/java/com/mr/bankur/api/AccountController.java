19394a5bde10e1469e04830b251c2abfdde246ef
Rain <rain.lillepuu@gmail.com>
2020-07-06 01:26:21 +0300

    AccountController added

---
+++ src/main/java/com/mr/bankur/api/AccountController.java
@@ -0,0 +1,17 @@
+package com.mr.bankur.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("account")
+@RestController
+public class AccountController {
+
+    /*
+    deposit
+    withdraw
+    getBalance
+    transfer
+     */
+
+}
